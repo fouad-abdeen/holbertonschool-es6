cfd11989d28d858ecb4db7543b57b57b0c1e6517
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-01-29 14:40:52 +0200

    Complete tasks

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,11 @@
+export default function createReportObject(employeesList) {
+  const obj = {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      const allKeys = Object.keys(employeesList);
+      return allKeys.length;
+    },
+  };
+
+  return obj;
+}
