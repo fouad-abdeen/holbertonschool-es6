a3c964450802768773a5bea284eab747d714e0a9
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-01-31 17:41:42 +0200

    Complete task-2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,6 +3,7 @@ export default function handleResponseFromAPI(promise) {
   promise.finally(() => {
     console.log('Got a response from the API');
   }).then(() => ({ status: 200, body: 'success' })).catch(() => {
-    throw new Error('');
+    throw new Error();
   });
+  return promise;
 }
