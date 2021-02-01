99aff2ed2237de9f5dff03cfc0bcfb9c87f78ef0
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-01 16:38:02 +0200

    Complete task-6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,5 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
-/* eslint-disable linebreak-style */
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
@@ -8,11 +7,7 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   try {
     const user = await signUpUser();
     const photo = await uploadPhoto();
-  } catch (e) {
-    console.log('Sign-Up failed!');
-  }
-  return [{
-    status: 'fullfiled',
-    value: 'value',
-  }];
+  // eslint-disable-next-line no-empty
+  } catch (e) { }
+  return Promise.allSettled([signUpUser(), uploadPhoto()]);
 }
