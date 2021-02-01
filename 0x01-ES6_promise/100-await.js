195f766896cd08aa12ca367b89ce6c86fc0da933
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-01 21:46:08 +0200

    Complete task-100

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return { photo, user };
+  } catch (e) {
+    return { photo: null, user: null };
+  }
+}
