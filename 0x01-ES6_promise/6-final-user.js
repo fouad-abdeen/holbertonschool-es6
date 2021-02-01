0bc9c22a2750fb4510f46903996e7a02478c4a4d
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-01 19:15:14 +0200

    Complete task-6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,6 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const user = await signUpUser();
-    const photo = await uploadPhoto();
-  // eslint-disable-next-line no-empty
-  } catch (e) { }
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(), uploadPhoto()]);
 }
