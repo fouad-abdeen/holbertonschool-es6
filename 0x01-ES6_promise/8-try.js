6382d56ef84ee12b7097e060b83998f7c8520742
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-03 00:31:35 +0200

    Complete task-8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,11 +1,7 @@
 /* eslint-disable linebreak-style */
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    }
-    return (numerator / denominator);
-  } catch (e) {
-    return e;
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
   }
+  return (numerator / denominator);
 }
