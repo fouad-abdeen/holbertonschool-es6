37adef579ecd5b68549152688bc2c91feacd0e79
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-03 00:29:05 +0200

    Complete task-8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -3,9 +3,8 @@ export default function divideFunction(numerator, denominator) {
   try {
     if (denominator === 0) {
       throw new Error('cannot divide by 0');
-    } else {
-      return (numerator / denominator);
     }
+    return (numerator / denominator);
   } catch (e) {
     return e;
   }
