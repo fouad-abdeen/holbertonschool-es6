c1b88faf7d3a20b0d3924232e02dca0f313ab808
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-02 15:02:32 +0200

    Complete task-1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,6 @@
 /* eslint-disable linebreak-style */
+import ClassRoom from './0-classroom';
+
 export default function initializeRooms() {
   const classRoom1 = new ClassRoom(19);
   const classRoom2 = new ClassRoom(20);
