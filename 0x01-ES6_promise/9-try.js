53542249264dcc07afc0b631b14fc0f1cc81fe79
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-03 00:36:11 +0200

    Complete task-9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,7 +1,13 @@
 /* eslint-disable linebreak-style */
 export default function guardrail(mathFunction) {
   const queue = [];
-  queue.push(mathFunction());
-  queue.push('Guardrail was processed');
+  try {
+    queue.push(mathFunction());
+  } catch(e) {
+    queue.push(e.toString());
+  }
+  finally {
+    queue.push('Guardrail was processed'); 
+  }
   return queue;
 }
