311aa2d6771cdf5e87164a0c027e3bbfb725588d
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-03 00:56:13 +0200

    Complete task-4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+/* eslint-disable linebreak-style */
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    this._currency = val;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
