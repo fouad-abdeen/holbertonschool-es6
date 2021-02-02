25e4937827acac2bd87afdf583807e534f6ed6ea
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-02-02 23:17:24 +0200

    Complete task-2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,11 @@
-/* eslint-disable linebreak-style */
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-  }).catch(() => {
-    throw new Error();
-  });
-  return ({ status: 200, body: 'success' });
+  promise
+    .then(() => ({ status: 200, body: "success" }))
+    .catch(() => {
+      throw new Error();
+    })
+    .finally(() => {
+      console.log("Got a response from the API");
+    });
+  return promise;
 }
